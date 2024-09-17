809e955d43f9272ea0d1043990bc815e59409fd0
erikvlcak <erikvlcak1@gmail.com>
2024-09-17 23:29:21 +0200

    add main modules

---
+++ excalibur.md
@@ -0,0 +1,5 @@
+Link to layout
+https://excalidraw.com/#json=1Mc61ZDCfzEEy-Vv1nkb5,OqdRRTfU9au9_6bCTqov9g
+
+Link to API
+https://www.themealdb.com/api.php
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import Meal from "./Meal.jsx";
+import Home from "./Home.jsx";
+import List from "./List.jsx";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Home />
+      <Meal />
+      <List />
+    </>
+  );
 }
